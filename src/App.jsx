1a6f9313fb2ce9e73b4208437181a10cc495bbd5
joaoVelosoT <vitorvteixeira2023@gmail.com>
2024-08-19 13:58:04 -0300

    criando repositorio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./Components/Header"
+import Card from "./Components/Card";
+import Banner from "./Components/Banner";
+import BannerCard from "./Components/BannerCard";
+import "./app.css";
+function App() {
+
+  return (
+    <>
+   <Header/>
+   <Banner/>
+   <BannerCard/>
+   <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/BannerCard.jsx
@@ -0,0 +1,13 @@
+import "./bannerCard.css";
+
+const BannerCard = () => {
+    return (
+        <div className="banner">
+            <h1>Banner principal</h1>
+            <h2>Lorem ipsum</h2>
+        </div>    
+    )
+}
+
+    
+export default BannerCard
